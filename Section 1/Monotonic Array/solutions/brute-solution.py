e9c1c72b30514c1f38405f1e44597b09501fc444
Dev. Sanches <dev.anaclarasanches@gmail.com>
2024-06-17 09:53:17 -0300

    algorithm explained

--- Section 1/Monotonic Array/solutions/brute-solution.py
+++ Section 1/Monotonic Array/solutions/brute-solution.py
@@ -1,3 +1,16 @@
+"""
+Uma lista monótona é exclusivamente não-crescente ou não-decrescente. Termos podem se repetir, mas essa lista não pode
+ser não-crescente e não-decrescente ao mesmo tempo. Repare que, para ser monótona, a lista pode assumir três formas:
+não-decrescente, não-crescente e uniforme. Para cada um destes casos, algumas condições precisam ser atendidas:
+- Não-decrescente: se o primeiro elemento da lista for menor que o último, significa que nenhum elemento anterior deve
+ser maior que o posterior. Caso contrário, a lista nõa é monótona.
+- Não-crescente: se o primeiro elemento da lista for maior que o último, significa que nenhum elemento anterior deve ser
+menor que o posterior. Caso contrário, a lista não é monótona.
+- Uniforme: se o primeiro elemento da lista for igual ao último, significa que todos os elementos da lista devem ser
+iguais. Caso contrário, a lista não é monótona.
+"""
+
+
 def monotonicArray(array):
     length = len(array)
     if length == 0 or length == 1:
@@ -13,7 +26,7 @@ def monotonicArray(array):
         for index in range(length - 1):
             if array[index] < array[index + 1]:
                 return False
-    # TUDO IGUAL
+    # UNIFORME
     else:
         for index in range(length - 1):
             if array[index] != array[index + 1]:
