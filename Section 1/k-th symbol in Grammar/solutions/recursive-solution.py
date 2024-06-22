46f634754224e97269a38f37c54ab82549d9f74d
Dev. Sanches <dev.anaclarasanches@gmail.com>
2024-06-22 11:32:08 -0300

    typos

--- Section 1/k-th symbol in Grammar/solutions/recursive-solution.py
+++ Section 1/k-th symbol in Grammar/solutions/recursive-solution.py
@@ -14,9 +14,9 @@ Propriedades da sequência:
 - Toda primeira metade da linha n é idêntica à primeira metade da linha n anterior (n - 1)
 - Toda segunda metade da linha n é idêntica ao inverso (NOT) da segunda metade da linha n anterior (n - 1)
 Levando em conta estas propriedades, para achar o valor de k em n é preciso verificar se k se encontra na primeira
-metade ou na segunda metade de n, agindo de acordo:
-- Se estiver na primeira metade de n, k é o próprio elemento de n anterior
-- se estiver na segunda metade de n, k é o elemento de n anterior inverso
+metade ou na segunda metade de n e agir de acordo:
+- Se estiver na primeira metade de n, k é o próprio elemento de n anterior (n - 1)
+- Se estiver na segunda metade de n, k é o elemento de n anterior inverso (n - 1)'
 
 Exemplo (k na segunda metade):
 n = 4, k = 7, mid (posição de k na linha anterior) = 7 // 4 = 3
